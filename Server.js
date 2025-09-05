ec90204a3fada334e8b241871c091d9d5a0c27f0
Muhammad Asad Ullah <masadullahkhanhfd@gmail.com>
2025-09-05 16:44:49 +0500

    Fix CORS for Vercel frontend

--- Server.js
+++ Server.js
@@ -10,11 +10,25 @@ const propertyRoute = require("./routes/propertyRoute")
 const inquiryRoutes = require("./routes/inquiryRoutes")
 
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://real-estate-website-frontend-brown.vercel.app',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, 
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
